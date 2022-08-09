743369214bd6a5e62ddf0df993d19aac073cff0b
Jireh <jeanxyz.012@gmail.com>
2022-08-09 09:43:27 +0800

    VERSION=100.26.10

--- lib/version/version.go
+++ lib/version/version.go
@@ -1,8 +1,8 @@
 package version
 
-const VERSION = "0.26.10"
+const VERSION = "100.26.10"
 
 // Compulsory minimum version, Minimum downward compatibility to this version
 func GetVersion() string {
-	return "0.26.0"
+	return "100.26.0"
 }
